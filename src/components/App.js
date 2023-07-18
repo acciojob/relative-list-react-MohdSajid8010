ac4be164f9ef130c843fbdaf01f8e5e73a1bbaf2
sajid <mohdsajid8010@gmail.com>
2023-07-18 11:38:25 +0530

    commit1

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+let arr = ["relativeListItem1", "relativeListItem2", "relativeListItem3", "relativeListItem4", "relativeListItem5",]
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <ol key="relativeList">
+        {
+          arr.map((el,i) => {
+            return (
+              <li key={el}>Relative {i+1}</li>
+            )
+          })
+        }
+      </ol>
+
     </div>
   )
 }
