44e25f1943348345d5f1e9f348cebd41d3c070b5
sajid <mohdsajid8010@gmail.com>
2023-07-18 12:28:33 +0530

    commit3

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,18 @@ const App = () => {
   return (
     <div id="main">
       <ol key="relativeList">
-        {
+        {/* {
           arr.map((el,i) => {
             return (
               <li key={el}>Relative {i+1}</li>
             )
           })
-        }
+        } */}
+        <li key="relativeListItem1">Relative 1</li>
+        <li key="relativeListItem2">Relative 2</li>
+        <li key="relativeListItem3">Relative 3</li>
+        <li key="relativeListItem4">Relative 4</li>
+
       </ol>
 
     </div>
